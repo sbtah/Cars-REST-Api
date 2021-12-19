c741d2b102da16e2a8c1c1f90604d3cbd3d96081
sbtah <shisastore@protonmail.com>
2021-12-19 22:10:43 +0100

    ...

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-)#_bq&8&6x-n@dtcbb&1!t(=3s7j#xhr68n$)u)m@vs20v-h+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
