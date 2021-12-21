f76c29783269c82eab9db146286aab988e3187de
sbtah <shisastore@protonmail.com>
2021-12-21 20:49:16 +0100

    Modified settings.py

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Car, Rate
+from .models import Car
 from django.shortcuts import get_object_or_404
 from .serializers import CarSerializer, RateSerializer, PopularCarSerializer
 
